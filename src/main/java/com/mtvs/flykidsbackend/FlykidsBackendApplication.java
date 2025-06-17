abc2a1255d507f03b5a5a9da57da9b6fdfb57fbf
전우선 <jeonwooseon@naver.com>
2025-06-17 14:15:24 +0900

    프로젝트 초기 세팅: Spring Boot 구성 완료

---
+++ src/main/java/com/mtvs/flykidsbackend/FlykidsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mtvs.flykidsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlykidsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlykidsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtvs/flykidsbackend/FlykidsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.flykidsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlykidsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
